91e857edb7875a3c5c0caeab5c9b27df69a8c2b1
unknown <G00387867@gmit.ie>
2020-02-17 20:46:17 +0000

    adding topic04-flow

---
+++ Topic04-flow/lab04.04-student.py
@@ -0,0 +1,20 @@
+# A program that reads in students
+# until the user enters a blank
+# and then prints them all out again
+
+students = []
+
+firstname = input("enter firstname (blank to quit): ").strip()
+while firstname != "":
+    student = {}
+    student["firstname"] = firstname
+    lastname = input("enter lastname: ").strip()
+    student["lastname"] = lastname
+    students.append(student)
+    # next student
+    firstname = input("enter firstname of next (blank to quit): ").strip()
+print ("here are the students you entered:")
+for currentstudent in students:
+    print("{} {} ".format(currentstudent["firstname"], 
+    currentstudent["lastname"]))
+    
